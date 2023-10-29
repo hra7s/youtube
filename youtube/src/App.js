0137119c5955bbcc71b1b362095a076c65904b9e
hra7s <216ec1464@gmail.com>
2023-10-29 10:24:41 +0530

    youtube clone

---
+++ youtube/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './components/Header';
+import Body from './components/Body';
+import { Provider } from 'react-redux';
+import store from './utils/store';
+function App() {
+  return (
+    <Provider store={store}>
+    <div className="">
+      <Header/>
+      <Body/>
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
