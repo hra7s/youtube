f5bfdedbd8961ca8b39e833ef868594965dbe436
hra7s <216ec1464@gmail.com>
2023-11-02 08:32:54 +0530

    clone-2

---
+++ youtube - Copy/src/components/ButtonsList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Button from './Button'
+
+const ButtonsList = () => {
+  
+  return (
+    <div className='flex'>
+      <Button name="All"/>
+      <Button name="Music"/>
+      <Button name="News"/>
+      <Button name="Live"/>
+      <Button name="Computer Programming"/>
+      <Button name="Cricket" />
+      <Button name="Cooking" />
+      <Button name="Dhee" />
+
+    </div>
+  )
+}
+
+export default ButtonsList
